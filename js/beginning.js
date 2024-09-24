c59ca77bc738639487249010a170eaac00b272d6
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2024-09-24 13:10:37 +0200

    comp

---
+++ js/beginning.js
@@ -0,0 +1,28 @@
+const a = 8;
+const huy = "huypizda";
+console.log(a);
+
+let username;
+console.log(typeof username); // "undefined"
+
+let inputValue = null;
+console.log(typeof inputValue); // "object"
+
+const quantity = 17;
+console.log(typeof quantity); // "number"
+
+const message = "JavaScript is awesome!";
+console.log(typeof message); // "string"
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // "boolean"
+
+const isComing = confirm("Please confirm hotel reservation");
+console.log(isComing);
+
+const hotelName = prompt("Please enter desired hotel name");
+console.log(hotelName);
+
+const value = prompt("Please enter a number!");
+console.log(typeof value); // "string"
+console.log(value); // "5"
